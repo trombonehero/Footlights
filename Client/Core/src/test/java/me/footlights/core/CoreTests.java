7bd4af72d7d3f6d600711307a0e26140d37a0a05
Jonathan Anderson <jonathan.anderson@cl.cam.ac.uk>
2011-06-17 12:59:48 +0100

    Do test everything under Core

--- Client/Core/src/test/java/me/footlights/core/CoreTests.java
+++ Client/Core/src/test/java/me/footlights/core/CoreTests.java
@@ -7,6 +7,9 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
 		{
+			me.footlights.core.crypto.FingerprintTest.class,
+			me.footlights.core.crypto.KeychainTests.class,
+			me.footlights.core.crypto.SecretKeyTest.class,
 			me.footlights.core.data.DataTests.class,
 			me.footlights.core.plugin.PluginTests.class
 		})

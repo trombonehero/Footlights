35ab14028602e788a5a76ee77435b87f0901c744
Jonathan Anderson <jonathan.anderson@ieee.org>
2011-08-11 00:39:08 +0100

    Provide a little bit more File implementation.
    Let File do useful things like present the data in its constituent Blocks as an InputStream.

--- Client/Core/src/main/java/me/footlights/core/data/File.java
+++ Client/Core/src/main/java/me/footlights/core/data/File.java
@@ -1,34 +1,120 @@
 package me.footlights.core.data;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
-import java.util.*;
+import java.security.GeneralSecurityException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
 
-/** A file */
+/**
+ * A logical file.
+ *
+ * Files are immutable; to modify a file, you must create and freeze a {@link MutableFile}.
+ */
 public class File
 {
-	/** Default constructor; produces an anonymous file */
-	public File()
+	public static MutableFile newBuilder() { return new MutableFile(); }
+	public static class MutableFile
 	{
-		plaintext = new LinkedList<Block>();
+		/** TODO: break content into appropriate-sized blocks. */
+		public synchronized MutableFile setContent(Iterable<ByteBuffer> content)
+		{
+			this.content.clear();
+			for (ByteBuffer b : content)
+				this.content.add(b.asReadOnlyBuffer());
+
+			return this;
+		}
+
+		/**
+		 * Produce a proper {@link File} by fixing the current contents of this
+		 * {@link MutableFile}.
+		 */
+		public synchronized File freeze() throws FormatException, GeneralSecurityException
+		{
+			List<Block> plaintext = Lists.newLinkedList();
+			List<EncryptedBlock> ciphertext = Lists.newLinkedList();
+
+			for (ByteBuffer b : content)
+			{
+				Block block = Block.newBuilder()
+					.setContent(b)
+					.build();
+
+				plaintext.add(block);
+				ciphertext.add(block.encrypt());
+			}
+
+			Block.Builder header = Block.newBuilder();
+			for (EncryptedBlock b : ciphertext) header.addLink(b.link());
+
+			return new File(header.build().encrypt(), plaintext, ciphertext);
+		}
+
+		private MutableFile() { this.content = new Vector<ByteBuffer>(); }
+
+		private final Vector<ByteBuffer> content;
 	}
 
 
-	/** Do we have the necessary authority to publish this file? */
-	public boolean isWritable() { return (publisher != null); }
-
-
-	/** Make a read-only copy of this file */
-	public File readOnly()
+	/**
+	 * The content of the file, transformed into an {@link InputStream}.
+	 */
+	public InputStream getInputStream()
 	{
-		File f = new File();
-		f.plaintext = plaintext;
-		f.publisher = null;
+		final ByteBuffer[] buffers = new ByteBuffer[plaintext.size()];
+		for (int i = 0; i < buffers.length; i++)
+			buffers[i] = plaintext.get(i).content();
 
-		return f;
+		return new InputStream()
+		{
+			@Override public int read(byte[] buffer, int offset, int len)
+			{
+				if (len == 0) return 0;
+				if (blockIndex >= plaintext.size()) return -1;
+
+				int pos = offset;
+				while (pos < len)
+				{
+					ByteBuffer next = buffers[blockIndex];
+					if (next.remaining() > 0)
+					{
+						int bytes = next.remaining();
+						next.get(buffer, pos, bytes);
+						pos += bytes;
+					}
+
+					if (pos == offset) return -1;
+					else blockIndex++;
+				}
+
+				return (pos - offset);
+			}
+
+			/** This is a horrendously inefficient way of reading data. Don't! */
+			@Override public int read() throws IOException
+			{
+				byte[] data = new byte[1];
+				int bytes = read(data, 0, data.length);
+
+				if (bytes < 0) throw new BufferUnderflowException();
+				if (bytes == 0)
+					throw new Error(
+						"Implementation error in File.read(byte[1]): returned 0");
+
+				return data[0];
+			}
+
+
+			private int blockIndex;
+		};
 	}
 
 
@@ -37,7 +123,7 @@ public class File
 	 *  
 	 * @throws IOException    on I/O errors such as network failures
 	 */
-	public List<ByteBuffer> content() throws IOException
+	List<ByteBuffer> content() throws IOException
 	{
 		List<ByteBuffer> content = Lists.newLinkedList();
 		for(Block b : plaintext) content.add(b.content());
@@ -45,10 +131,26 @@ public class File
 		return content;
 	}
 
+	/** Encrypted blocks to be saved in a {@link Store}. */
+	List<EncryptedBlock> toSave()
+	{
+		LinkedList<EncryptedBlock> everything = Lists.newLinkedList(ciphertext);
+		everything.push(header);
 
-	private List<Block> plaintext;
-	private List<Block> encrypted;
+		return everything;
+	}
 
-	/** We'll figure out how this actually works later */
-	Object publisher;
+	/** Default constructor; produces an anonymous file */
+	private File(EncryptedBlock header,
+		List<Block> plaintext, List<EncryptedBlock> ciphertext)
+	{
+		this.header = header;
+		this.plaintext = ImmutableList.<Block>builder().addAll(plaintext).build();
+		this.ciphertext = ImmutableList.<EncryptedBlock>builder().addAll(ciphertext).build();
+	}
+
+
+	private final EncryptedBlock header;
+	private final ImmutableList<Block> plaintext;
+	private final ImmutableList<EncryptedBlock> ciphertext;
 }
--- Client/Core/src/test/java/me/footlights/core/data/FileTest.java
+++ Client/Core/src/test/java/me/footlights/core/data/FileTest.java
@@ -1,17 +1,59 @@
 package me.footlights.core.data;
 
+import java.nio.ByteBuffer;
+import java.util.List;
+
 import me.footlights.core.data.File;
 
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 
 public class FileTest
 {
-	@Test public void anonymousFile() throws Throwable
+	@Test public void emptyFile() throws Throwable
 	{
-		File f = new File();
-
+		File f = File.newBuilder().freeze();
 		assertEquals(0, f.content().size());
+		assertEquals(1, f.toSave().size());
+	}
+
+	@Test public void singleBlock() throws Throwable
+	{
+		byte[] rawData = new byte[] { 1, 2, 3, 4 };
+
+		List<ByteBuffer> data = Lists.newArrayList();
+		data.add(ByteBuffer.wrap(rawData));
+
+		File f = File.newBuilder()
+			.setContent(data)
+			.freeze();
+
+		assertEquals(1, f.content().size());
+		assertEquals(2, f.toSave().size());
+
+		byte[] dataOut = new byte[rawData.length];
+		f.content().get(0).get(dataOut);
+		assertArrayEquals(rawData, dataOut);
+	}
+
+	/** Ensure that a file, composed of several blocks, can be viewed as a seamless unit. */
+	@Test public void seamlessView() throws Throwable
+	{
+		byte[] orig = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+		ByteBuffer b1 = ByteBuffer.wrap(orig, 0, 3);
+		ByteBuffer b2 = ByteBuffer.wrap(orig, 3, 5);
+		List<ByteBuffer> data = Lists.newArrayList(b1, b2);
+
+		File f = File.newBuilder().setContent(data).freeze();
+
+		byte[] copy = new byte[orig.length];
+		int bytes = f.getInputStream().read(copy);
+		assertEquals(orig.length, bytes);
+		assertArrayEquals(orig, copy);
 	}
 }

c53d64765fecc5950f8bd645891d18beacd5acc4
Jonathan Anderson <jonathan.anderson@ieee.org>
2011-09-05 15:14:34 +0100

    Simplify ajax.js.
    Since all JavaScript responses now come with mime-type 'text/javascript', we no longer need the "parse JavaScript out of XML" code, nor defaultHandler().

--- Client/UI/Web/src/main/resources/me/footlights/ui/web/ajax.js
+++ Client/UI/Web/src/main/resources/me/footlights/ui/web/ajax.js
@@ -4,13 +4,12 @@ if(window.XMLHttpRequest) { xhr = new XMLHttpRequest(); }
 else if(window.ActiveXObject) { xhr = new ActiveXObject("Microsoft.XMLHTTP"); }
 
 
-function ajax(request, context) { ajaxWithCallback(request, context, defaultCallback); }
-function ajaxWithCallback(url, context, callback)
+function ajax(url, context)
 {
 	try
 	{
 		xhr.open('GET', url);
-		xhr.onreadystatechange = function() { handleAjax(xhr, url, context, callback); };
+		xhr.onreadystatechange = function() { handleAjax(xhr, url, context); };
 
 		xhr.open('GET', 'http://localhost:4567/' + url, true);
 		xhr.send(null);
@@ -19,7 +18,7 @@ function ajaxWithCallback(url, context, callback)
 }
 
 
-function handleAjax(xhr, request, context, callback)
+function handleAjax(xhr, request, context)
 {
 	if(xhr.readyState == 4)
 	{
@@ -36,8 +35,7 @@ function handleAjax(xhr, request, context, callback)
 				var type = doc.getElementsByTagName('type')[0].childNodes[0].nodeValue;
 				var content = doc.getElementsByTagName('content')[0].childNodes[0].nodeValue;
 
-				if(type == "code") callback(context, content);
-				else if(type == "error") showError(context, content);
+				if(type == "error") showError(context, content);
 				else showAjaxResponse(type, content);
 
 				return;
@@ -47,10 +45,4 @@ function handleAjax(xhr, request, context, callback)
 				return;
 		}
 	}
-}
-
-function defaultCallback(context, content)
-{
-	context.log('got code for context "' + context.name + '"');
-	context.exec(content);
 }

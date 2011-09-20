ab251d513cc7ffca72d493d1ca1e40daa5c4d393
Jonathan Anderson <jonathan.anderson@ieee.org>
2011-09-20 11:40:10 +0100

    Use the existing JARLoader to cut code.

--- Client/Bootstrap/src/main/java/me/footlights/boot/PluginClassLoader.java
+++ Client/Bootstrap/src/main/java/me/footlights/boot/PluginClassLoader.java
@@ -17,28 +17,14 @@ package me.footlights.boot;
 
 import java.io.FilePermission;
 import java.io.IOException;
-import java.io.InputStream;
-
-import java.net.JarURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
-import java.security.AccessController;
-import java.security.CodeSource;
 import java.security.Permissions;
-import java.security.PrivilegedAction;
 import java.security.ProtectionDomain;
 
-import java.util.Enumeration;
 import java.util.Map;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
 
 import com.google.common.collect.Maps;
-
-import me.footlights.core.Preconditions;
 
 
 /** Loads unprivileged plugin code. */
@@ -48,84 +34,71 @@ class PluginClassLoader extends ClassLoader
 	{
 		this.coreLoader  = coreLoader;
 		pluginClasses    = Maps.newHashMap();
-		packageURLs      = Maps.newHashMap();
+		pluginJars      = Maps.newHashMap();
 	}
 
 
 	/**
-	 * Find a class, given the class name.
+	 * Find a plugin class, given the class name or an explicit JAR file + class name.
 	 * 
 	 * @name   The name of the class, either in the usual format (package.Class)
 	 *         or in Jar-like format:
-	 *         jar:/http://host.com/path/to/plugin.jar!/packagename.Classname
+	 *         jar:/http://host.com/path/to/plugin.jar!/packagename.Classname.
+	 *         If using the usual package.Class format with implicit JAR file, another class
+	 *         must have been previously loaded from the same package with an explicit JAR file. 
 	 */
 	@Override protected synchronized Class<?> findClass(String name)
 		throws ClassNotFoundException
 	{
 		if (name.startsWith("me.footlights")) return coreLoader.findClass(name);
 
-		// have we already loaded this plugin?
+		// Have we already loaded this plugin?
 		if (pluginClasses.containsKey(name)) return pluginClasses.get(name);
 
+		// Find the JAR file, whether it's given implicitly or explicitly, and class name.
+		final JARLoader jar;
+		String className;
+
 		String[] parts = name.split("\\.jar!/");
-		if (parts.length > 1 && pluginClasses.containsKey(parts[1]))
-			return pluginClasses.get(parts[1]);
-
-
-		// load it
 		if (parts.length == 2)
 		{
-			try
-			{
-				URL jarURL = new URL(parts[0] + ".jar!/");
-				String className = parts[1];
-	
-				return findPluginClass(jarURL, className);
-			}
-			catch(MalformedURLException e) { throw new Error(e); }
+			// Both the JAR file and class name have been given explicitly.
+			try { jar = new JARLoader(new URL(parts[0] + ".jar!/")); }
+			catch (IOException e) { throw new ClassNotFoundException("Error reading JAR", e); }
+
+			className = parts[1];
 		}
 		else if (parts.length == 1)
 		{
+			// Only the class name is explicit; the JAR file must be implicit.
+			className = parts[0];
+
 			int lastDot = name.lastIndexOf(".");
 			String packageName = name.substring(0, lastDot);
-			URL packageURL = packageURLs.get(packageName);
+			jar = pluginJars.get(packageName);
 
-			if (packageURL == null)
-				throw new ClassNotFoundException(
-					"Unknown package '" + packageName + "'");
-
-			return findPluginClass(packageURLs.get(packageName), name);
+			if (jar == null)
+				throw new ClassNotFoundException("Unknown package '" + packageName + "'");
 		}
-		else throw new ClassNotFoundException("Invalid plugin class: " + name);
-	}
+		else throw new ClassNotFoundException("Invalid class name: '" + name + "'");
 
 
-	/**
-	 * Find a plugin class from a Jar file.
-	 * 
-	 * @param   url            the Jar's URL (starts with jar:/, etc.)
-	 * @param   className      the class' name
-	 */
-	public synchronized Class<?> findPluginClass(URL url, String className)
-		throws ClassNotFoundException
-	{
-		Preconditions.notNull(url);
+		// Read and define the class.
+		Bytecode bytecode;
+		try { bytecode = jar.readBytecode(className); }
+		catch (IOException e) { throw new ClassNotFoundException("Error reading JAR", e); }
 
-		// first, get the bytecode
-		Bytecode bytecode = readBytecodeFromJAR(url, className);
 		String sourceURL = bytecode.source.getLocation().toString();
 		if (sourceURL.startsWith("file:"))
 			sourceURL = sourceURL.replace("file:", "");
 
 		Permissions permissions = new Permissions();
 		permissions.add(new FilePermission(sourceURL, "read"));
-
-		ProtectionDomain domain
-			= new ProtectionDomain(bytecode.source, permissions);
+		ProtectionDomain domain = new ProtectionDomain(bytecode.source, permissions);
 
 		int lastDot = className.lastIndexOf(".");
 		String packageName = className.substring(0, lastDot);
-		packageURLs.put(packageName, url);
+		pluginJars.put(packageName, jar);
 
 		Class<?> c = defineClass(className, bytecode.raw, 0, bytecode.raw.length, domain);
 		pluginClasses.put(className, c);
@@ -134,132 +107,12 @@ class PluginClassLoader extends ClassLoader
 	}
 
 
-
-	/**
-	 * Read bytecode from a JAR file.
-	 * 
-	 * @param   url           URL of the JAR file to read from
-	 * @param   className     the name of the class whose bytecode we're loading
-	 * 
-	 * @return  a Bytecode object
-	 * 
-	 * @throws  ClassNotFoundException
-	 */
-	Bytecode readBytecodeFromJAR(URL url, String className)
-		throws ClassNotFoundException
-	{
-		JarFile jar = null;
-
-		try
-		{
-			jar = getJarFile(url);
-
-			Manifest man = jar.getManifest();
-			if (man == null)
-				throw new SecurityException("The jar file is not signed");
-
-			// load our entry
-			for (Enumeration<JarEntry> i = jar.entries() ; i.hasMoreElements() ;)
-			{
-				JarEntry entry = i.nextElement();
-
-				if (entry.isDirectory()) continue;
-				if (entry.getName().startsWith("META-INF/")) continue;
-	
-				// read the JAR entry (to make sure it's actually signed)
-				InputStream is = jar.getInputStream(entry);
-				int avail = is.available();
-				byte[] buffer = new byte[avail];
-				is.read(buffer);
-				is.close();
-
-				if (entry.getName().equals(className.replace('.', '/') + ".class"))
-				{
-					if (entry.getCodeSigners() == null)
-						throw new Error(entry.toString() + " not signed");
-	
-					String jarName = url.toExternalForm();
-					jarName = jarName.replaceFirst("jar:", "");
-					jarName = jarName.replace("!/", "");
-	
-					Bytecode bytecode = new Bytecode();
-					bytecode.raw = buffer;
-					bytecode.source = new CodeSource(new URL(jarName), entry.getCodeSigners());
-	
-					return bytecode;
-				}
-			}
-
-			throw new ClassNotFoundException(
-				jar + " does not contain " + className);
-		}
-		catch (IOException e)
-		{
-			throw new ClassNotFoundException(
-				"Error loading " + className + " from URL " + url, e);
-		}
-	}
-
-
-
-	/**
-	 * Turn a URL into a Jar file.
-	 */
-	private JarFile getJarFile(URL url) throws IOException
-	{
-		Preconditions.notNull(url);
-
-		JarFile jar;
-
-		if (url.toString().startsWith("jar:file:") || url.toString().startsWith("file:"))
-		{
-			String fileURL = url.toString();
-			if (fileURL.startsWith("jar:file:"))
-				fileURL = fileURL.replaceFirst("jar:file:", "");
-
-			else if (fileURL.startsWith("file:"))
-				fileURL = fileURL.replaceFirst("file:", "");
-
-
-			if (fileURL.startsWith("///"))
-				fileURL = fileURL.replaceFirst("///", "/");
-
-			fileURL = fileURL.replaceFirst("!/$", "");
-
-			final String finalFileURL = fileURL;
-			jar =
-				AccessController.doPrivileged(new PrivilegedAction<JarFile>()
-					{
-						public JarFile run()
-						{
-							try { return new JarFile(finalFileURL); }
-							catch (IOException e) { throw new Error(e); }
-						}
-					});
-		}
-		else
-		{
-			final URLConnection connection = url.openConnection();
-			connection.setUseCaches(false);
-
-			jar = 
-				AccessController.doPrivileged(new PrivilegedAction<JarFile>()
-				{
-					public JarFile run()
-					{
-						try { return ((JarURLConnection) connection).getJarFile(); }
-						catch(IOException e) { throw new Error(e); }
-					}
-				});
-		}
-		return jar;
-	}
-
-
 	/** {@link ClassLoader} for core Footlights classes. */
 	private final FootlightsClassLoader coreLoader;
 
 	/** Plugins we've already loaded */
-	private final Map<String,Class<?>> pluginClasses;
-	private final Map<String,URL> packageURLs;
+	private final Map<String, Class<?>> pluginClasses;
+
+	/** Plugin JAR files. */
+	private final Map<String, JARLoader> pluginJars;
 }

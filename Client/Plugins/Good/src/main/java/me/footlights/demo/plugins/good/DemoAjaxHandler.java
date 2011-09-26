947270a34cbbdf751383029c87bfcf766efe56d7
Jonathan Anderson <jon@footlights.me>
2011-09-26 14:04:45 +0100

    Use the Context API in DemoAjaxHandler.

--- Client/Plugins/Good/src/main/java/me/footlights/demo/plugins/good/DemoAjaxHandler.java
+++ Client/Plugins/Good/src/main/java/me/footlights/demo/plugins/good/DemoAjaxHandler.java
@@ -1,5 +1,6 @@
 package me.footlights.demo.plugins.good;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Date;
@@ -7,14 +8,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import me.footlights.plugin.AjaxHandler;
+import me.footlights.plugin.Context;
 import me.footlights.plugin.File;
 import me.footlights.plugin.JavaScript;
 import me.footlights.plugin.KernelInterface;
 import me.footlights.plugin.WebRequest;
 
+import static me.footlights.demo.plugins.good.DemoAjaxHandler.AjaxRequest.*;
+
 
 /** A demonstration of what a {@link Plugin}'s {@link AjaxHandler} can do. */
-class DemoAjaxHandler implements AjaxHandler
+class DemoAjaxHandler extends Context
 {
 	enum AjaxRequest
 	{
@@ -26,46 +30,47 @@ class DemoAjaxHandler implements AjaxHandler
 		CLICKED,
 	}
 
-	DemoAjaxHandler(KernelInterface kernel, Logger log)
+	DemoAjaxHandler(final KernelInterface kernel, final Logger log)
 	{
-		this.kernel = kernel;
-		this.log = log;
-	}
+		register(INIT.name().toLowerCase(), new AjaxHandler()
+			{
+				@Override public JavaScript service(WebRequest request)
+					throws FileNotFoundException, SecurityException, Throwable
+				{
+					return new JavaScript()
+						.append(makeDiv("Initializing well-behaved plugin."))
+						.append(makeDiv("The time is " + new Date()))
+						.append(ajax(AjaxRequest.TEST_METHODS.name()));
+				}
+			});
 
-	@Override
-	public JavaScript service(WebRequest request)
-	{
-		JavaScript response = new JavaScript();
-
-		response.append(hr());
-		response.append(makeDiv("Acting in response to request '" + request.path() + "'"));
-
-		final AjaxRequest ajax;
-		try { ajax = AjaxRequest.valueOf(request.prefix().toUpperCase()); }
-		catch (IllegalArgumentException e)
+		register(TEST_METHODS.name(), new AjaxHandler()
 		{
-			return response.append(
-				"log.log('Unknown request \\'" + JavaScript.sanitizeText(request.path()) + "\\'')");
-		}
+			@Override public JavaScript service(WebRequest request)
+				throws FileNotFoundException, SecurityException, Throwable
+			{
+				JavaScript response = new JavaScript();
 
-		switch (ajax)
-		{
-			case INIT:
-				response.append(makeDiv("Initializing well-behaved plugin."));
-				response.append(makeDiv("The time is " + new Date()));
-				response.append(ajax(AjaxRequest.TEST_METHODS.name()));
-				break;
-
-			case TEST_METHODS:
 				response.append(
 					makeDiv("Test static method in the Helper class... " + Helper.staticHelp()));
-					response.append(makeDiv("Ok, that was fine. Now a constructor... "));
-					Helper h = new Helper();
-					response.append(makeDiv("And a regular method... "+ h.help()));
-					response.append(ajax(AjaxRequest.SYSCALL.name()));
-					break;
 
-			case SYSCALL:
+				response.append(makeDiv("Ok, that was fine. Now a constructor... "));
+				Helper h = new Helper();
+
+				response.append(makeDiv("And a regular method... "+ h.help()));
+
+				response.append(ajax(AjaxRequest.SYSCALL.name()));
+				return response;
+			}
+		});
+
+		register(SYSCALL.name(), new AjaxHandler()
+		{
+			@Override public JavaScript service(WebRequest request)
+				throws FileNotFoundException, SecurityException, Throwable
+			{
+				JavaScript response = new JavaScript();
+
 				response.append(makeDiv("Finally, do a 'syscall'..."));
 				if (kernel == null)
 					response.append(makeDiv("but we can't! our kernel reference is null."));
@@ -84,35 +89,48 @@ class DemoAjaxHandler implements AjaxHandler
 				}
 
 				response.append(ajax(AjaxRequest.CONTENT.name()));
-				break;
+				return response;
+			}
+		});
 
-			case CONTENT:
-				response.append("context.load('test.js');");
-				/*
-				 * TODO: fix ajax.js so that we have more than one XHR object.
-				 *
-				 * Until this is done, sending ajax('ALL_DONE') will cancel load('test.js'),
-				 * leading to a rather less convincing demo.
-				 */
-//				response.append(ajax(AjaxRequest.ALL_DONE.name()));
-//				break;
+		register(CONTENT.name(), new AjaxHandler()
+		{
+			@Override public JavaScript service(WebRequest request)
+				throws FileNotFoundException, SecurityException, Throwable
+			{
+				return new JavaScript()
+					.append("context.load('test.js');")
+					/*
+					 * TODO: fix ajax.js so that we have more than one XHR object.
+					 *
+					 * Until this is done, sending ajax('ALL_DONE') will cancel load('test.js'),
+					 * leading to a rather less convincing demo.
+					 */
+//					.append(ajax(AjaxRequest.ALL_DONE.name()))
+					;
+			}
+		});
 
-			case ALL_DONE:
-				response.append(makeDiv("The plugin works!."));
-				break;
+		register(ALL_DONE.name(), new AjaxHandler()
+		{
+			@Override public JavaScript service(WebRequest request)
+				throws FileNotFoundException, SecurityException, Throwable
+			{
+				return new JavaScript().append(makeDiv("The plugin works!."));
+			}
+		});
 
-			case CLICKED:
-				response.append("context.log('Clicked \\'" + request.shift().path() + "\\'');");
-				break;
-		}
-
-		return response;
+		register(CLICKED.name().toLowerCase(), new AjaxHandler()
+		{
+			@Override public JavaScript service(WebRequest request)
+				throws FileNotFoundException, SecurityException, Throwable
+			{
+				return new JavaScript()
+					.append("context.log('Clicked \\'" + request.shift().path() + "\\'');");
+			}
+		});
 	}
 
-	private static JavaScript hr()
-	{
-		return new JavaScript().append("context.root.appendElement('hr')");
-	}
 
 	private static JavaScript makeDiv(String text)
 	{
@@ -130,7 +148,4 @@ class DemoAjaxHandler implements AjaxHandler
 			.append(JavaScript.sanitizeText(command))
 			.append("')");
 	}
-
-	private final KernelInterface kernel;
-	private final Logger log;
 }

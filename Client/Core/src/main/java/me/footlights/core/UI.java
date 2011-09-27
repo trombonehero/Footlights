113342f497a98ebe7da34bb6856510622e82afcf
Jonathan Anderson <jon@footlights.me>
2011-09-27 14:18:44 +0100

    Don't actually need this.

--- Client/Core/src/main/java/me/footlights/core/UI.java
+++ Client/Core/src/main/java/me/footlights/core/UI.java
@@ -30,16 +30,10 @@ public abstract class UI extends Thread implements PluginWatcher
 	public UI(String name, Footlights footlights)
 	{
 		super("Footlights UI: '" + name + "'");
-
-		this.footlights = footlights;
 		footlights.registerUI(this);
 	}
 
 
 	public void pluginLoaded(PluginWrapper plugin) {}
 	public void pluginUnloading(PluginWrapper plugin) {}
-
-
-	/** Reference to the core */
-	protected Footlights footlights;
 }

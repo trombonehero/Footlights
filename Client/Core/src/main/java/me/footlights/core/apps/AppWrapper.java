64a67800162e7655743d08475c18440b110a2205
Jonathan Anderson <jon@footlights.me>
2012-02-22 14:36:39 +0000

    Add AppWrapper.toString().

--- Client/Core/src/main/java/me/footlights/core/apps/AppWrapper.java
+++ Client/Core/src/main/java/me/footlights/core/apps/AppWrapper.java
@@ -38,6 +38,8 @@ public final class AppWrapper
 	public Application getApp() { return app; }
 
 
+	@Override public String toString() { return "Application { '" + name + "' from " + url + " }"; }
+
 	/** The actual app. */
 	private final Application app;
 

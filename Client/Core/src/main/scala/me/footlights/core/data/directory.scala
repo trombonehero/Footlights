1509d6ac7983b36f0207a121b55e9c32d399dae8
Jonathan Anderson <jon@footlights.me>
2012-03-29 15:46:55 +0100

    Add a Directory to point at files and directories.
    A Directory is just a mapping from arbitrary names to arbitrary Link objects, with the provision for directory/file labelling.

---
+++ Client/Core/src/main/scala/me/footlights/core/data/directory.scala
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2012 Jonathan Anderson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.nio.ByteBuffer
+import java.nio.channels.Channels
+
+import scala.collection.JavaConversions._
+
+import me.footlights.api
+import me.footlights.core
+
+
+package me.footlights.core.data {
+
+class Entry(val name:String, val isDir:Boolean, val link:Link) {
+	val isFile = !isDir
+
+	override lazy val toString = name + (if (isDir) "/" else "")
+	override def equals(a:Any) = {
+		if (!a.isInstanceOf[Entry]) false
+		else {
+			val other = a.asInstanceOf[Entry]
+			(other.name == name) && (other.isDir == isDir) && (other.link == link)
+		}
+	}
+}
+
+/**
+ * Provides a name -> file mapping
+ */
+class Directory(private val entries:Map[String,Entry]) {
+	import Directory._
+
+	def + (mapping:Entry) = Directory { entries + (mapping.name -> mapping) }
+
+	def ++ (dir:Directory) = Directory { entries ++ dir.entries }
+
+	def apply(name:String) = entries get name
+
+	lazy val link = encrypted.head.link
+
+	override def equals(a:Any) = {
+		if (!a.isInstanceOf[Directory]) false
+		else a.asInstanceOf[Directory].entries == entries
+	}
+
+	override lazy val toString =
+		if (entries.isEmpty) "Directory()"
+		else "Directory(%s)" format (entries.values map { _.toString } reduce { _ + ", " + _ })
+
+	lazy val encrypted:Iterable[EncryptedBlock] = {
+		if (entries.size > Short.MaxValue)
+			throw new FormatException("Cannot store %d directory entries" format entries.size)
+
+		// Get byte-level representation of directory entries.
+		val entryBytes = entries.values map { entry =>
+			val name = entry.name.getBytes
+			val size = entry.link.bytes + name.length
+
+			(size, name, entry.isDir, entry.link)
+		} toSeq
+
+
+		// Build content blocks in reverse order, adding entries until they are full.
+		var ciphertext = List[EncryptedBlock]()
+		var blocks = List(
+			Block.newBuilder setDesiredSize BlockSize addContent Directory.Terminator.toArray
+		)
+
+		def createNewBlock = {
+			val oldHead = blocks.head
+			val encrypted = oldHead.build.encrypt
+
+			// The first link is to the next block.
+			val b =
+				Block.newBuilder
+					.setDesiredSize(BlockSize)
+					.addContent(Directory.Magic toArray)
+					.addLink(encrypted.link)
+
+			blocks ::= b
+			ciphertext ::= encrypted
+
+			b
+		}
+
+		// Walk through directory entries in reverse order, adding them to blocks.
+		//
+		// There's no reason we couldn't do another ordering, putting the most-shared things
+		// at the end of the block chain and allowing sharing of partial directories.
+		//
+		// Future work.
+		var current = blocks.head
+		entryBytes.reverseIterator foreach { case Tuple4(size, name, isDir, link) =>
+			if (size > current.remaining) current = createNewBlock
+
+			if (name.length > (Short.MaxValue / 2))
+				throw new FormatException("Name too long (%d B): '%s[...]'" format (
+						name.length, new String(name, 0, 30)
+					))
+			val namelen = (name.length.toShort & 0x7FFF) + (if (isDir) 0x8000 else 0) toShort
+
+			val entry = ByteBuffer allocate name.length + 2
+			entry putShort namelen
+			entry put name
+			entry flip
+
+			current addLink link
+			current addContent entry
+		}
+
+		current.build.encrypt :: ciphertext
+	}
+
+	private val BlockSize = 4096   // TODO: something more generic
+}
+
+object Directory {
+	def apply(entries:Map[String,Entry] = Map()) = new Directory(entries)
+
+	def parse(blocks:Iterable[Block]): Directory = {
+		var entries = blocks map { block =>
+			val content = block.content
+			val magic = new Array[Byte](Magic.length)
+			content get magic
+
+			val isTerminator = magic.toSeq match {
+				case Terminator => true
+				case Magic => false
+				case other:Any =>
+					throw new FormatException("Invalid directory magic '%s'" format other)
+			}
+
+			val links:Iterable[Link] = if (isTerminator) block.links else block.links.tail
+			for (link <- links) yield {
+				val namelen = content getShort
+				val isDir = (namelen & 0x8000) == 0x8000
+				val name = new Array[Byte](namelen & 0x7FFF)
+				content get name
+
+				new Entry(new String(name), isDir, link)
+			}
+		} reduce { _ ++ _ } map { entry => (entry.name -> entry) } toMap
+
+		new Directory(entries)
+	}
+
+	implicit def file2entry(x:(String,File)) = Entry(x._1, x._2)
+	implicit def dir2entry(x:(String,Directory)) = Entry(x._1, x._2)
+
+	/** Magic constant for a Footlights directory: vaguely "FOOTDIR" in hexaleet. */
+	private val Magic = Seq(0xF0, 0x07, 0xD1, 0x12) map { _.toByte }
+
+	/** Special magic for the last block in the directory chain. */
+	private val Terminator = Seq(0xF0, 0x07, 0xD1, 0x13) map { _.toByte }
+}
+
+object Entry {
+	def apply(name:String, file:File) = new Entry(name, isDir = false, file.link)
+	def apply(name:String, dir:Directory) = new Entry(name, isDir = true, dir.link)
+}
+
+}
---
+++ Client/Core/src/test/scala/me/footlights/core/data/directory-test.scala
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2012 Jonathan Anderson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.nio.ByteBuffer
+import java.security.NoSuchAlgorithmException
+
+import scala.collection.JavaConversions._
+
+import org.junit.runner.RunWith
+
+import org.mockito.Matchers._
+import org.mockito.Mockito.when
+
+import org.powermock.api.mockito.PowerMockito
+
+import org.scalatest.{BeforeAndAfter,FreeSpec}
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.mock.MockitoSugar
+
+import me.footlights.core.crypto
+import me.footlights.core.data.store._
+import me.footlights.core.security
+
+
+package me.footlights.core.data {
+
+import Directory._
+
+@RunWith(classOf[JUnitRunner])
+class DirectoryTest extends FreeSpec with BeforeAndAfter with MockitoSugar with ShouldMatchers {
+	"A Directory should be able to " - {
+		"store files." in {
+			val d = Directory() + ("foo" -> file1) + ("bar" -> file2)
+
+			for ((name, file) <- List("foo" -> file1, "bar" -> file2)) {
+				val f = d(name)
+				f should equal (Some(Entry(name, file)))
+				f.get.link should equal (file.link)
+			}
+		}
+
+		"store directories." in {
+			val d = Directory() + ("foo" -> dir1) + ("bar" -> dir2)
+
+			for ((name, dir) <- List("foo" -> dir1, "bar" -> dir2)) {
+				val f = d(name)
+				f should equal (Some(Entry(name, dir)))
+				f.get.link should equal (dir.link)
+			}
+		}
+
+		"store files and directories." in {
+			val d = Directory() + ("foo" -> file1) + ("bar" -> dir1)
+
+			d("foo") should equal (Some(Entry("foo", file1)))
+			d("bar") should equal (Some(Entry("bar", dir1)))
+		}
+
+		"produce a parseable byte-level representation." in {
+			val d = Directory() + ("foo" -> file1) + ("bar" -> dir1)
+			val encrypted = d.encrypted
+			encrypted.toList.length should be > (0)
+
+			var link = encrypted.head.link
+			val decrypted = encrypted map { e =>
+				val plaintext = link decrypt e.ciphertext
+				link = plaintext.links.head
+				plaintext
+			}
+
+			val parsed = Directory parse decrypted
+			parsed should equal (d)
+		}
+	}
+
+	private val link1 = Link.newBuilder
+		.setFingerprint(crypto.Fingerprint.newBuilder setContent("link1".getBytes) build)
+		.setKey(crypto.SecretKey.newGenerator.generate)
+		.build
+
+	private val dir1 = mock[Directory]
+	private val file1 = mock[File]
+	when { dir1.link } thenReturn link1
+	when { file1.link } thenReturn link1
+
+	private val link2 = Link.newBuilder
+		.setFingerprint(crypto.Fingerprint.newBuilder setContent("link2".getBytes) build)
+		.setKey(crypto.SecretKey.newGenerator.generate)
+		.build
+
+	private val dir2 = mock[Directory]
+	private val file2 = mock[File]
+	when { dir2.link } thenReturn link1
+	when { file2.link } thenReturn link2
+}
+
+}

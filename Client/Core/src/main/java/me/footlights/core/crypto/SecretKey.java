71c67adbf017e74eac1f8921aeaf8152c4f79b21
Jonathan Anderson <jon@footlights.me>
2012-04-28 22:31:31 +0100

    Don't do this test here.
    This was a debugging thing, at it breaks test code. If we really want to check that an algorithm is valid when e.g. parsing a block, we should check it there.

--- Client/Core/src/main/java/me/footlights/core/crypto/SecretKey.java
+++ Client/Core/src/main/java/me/footlights/core/crypto/SecretKey.java
@@ -107,8 +107,6 @@ public class SecretKey
 
 			if (keySpec == null) keySpec = new SecretKeySpec(secret, algorithm);
 
-			// Can we understand this algorithm?
-			javax.crypto.Cipher.getInstance(keySpec.getAlgorithm());
 			return new SecretKey(keySpec, fingerprint.setContent(secret).build());
 		}
 

c76b97e78a450e85ced74cdd9ce3b14267a6824e
Jonathan Anderson <jon@footlights.me>
2012-05-01 18:50:00 +0100

    Trivial: style nit.

--- API/src/main/scala/me/footlights/api/support/pipeline.scala
+++ API/src/main/scala/me/footlights/api/support/pipeline.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package me.footlights.api.support;
+package me.footlights.api.support
 
 
 object Pipeline {
